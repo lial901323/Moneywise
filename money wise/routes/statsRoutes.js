3bbb5610f9f404d102d924394fe27faa70eb1ef5
lial lafi <148901305+lial901323@users.noreply.github.com>
2025-07-28 18:56:10 +0300

    Update statsRoutes.js

--- money wise/routes/statsRoutes.js
+++ money wise/routes/statsRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { protect } = require('../middleware/authMiddleware');
+const  protect  = require('../middleware/authMiddleware');
 const Deposit = require('../models/depositModel');
 const Expense = require('../models/expenseModel');
 const User = require('../models/User');
