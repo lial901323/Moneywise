921ad02e30fcce15c4f0f1fbef1dffb117923f40
lial lafi <148901305+lial901323@users.noreply.github.com>
2025-07-30 18:29:28 +0300

    Delete money wise directory

--- money wise/middleware/roleMiddleware.js
+++
@@ -1,8 +0,0 @@
-module.exports = (requiredRole) => {
-  return (req, res, next) => {
-    if (req.user.role !== requiredRole) {
-      return res.status(403).json({ message: "Access denied: Admins only" });
-    }
-    next();
-  };
-};
